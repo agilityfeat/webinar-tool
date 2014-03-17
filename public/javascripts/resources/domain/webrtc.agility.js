3827f58e7048fd91d52cc8c91d61c0faa18b77bc
asime <arin@arinsime.com>
2014-03-16 20:10:09 -0400

    updating layout to show presentation view

---
+++ public/javascripts/resources/domain/webrtc.agility.js
@@ -0,0 +1,132 @@
+//(function(w,d,$, _){
+
+	var agility_webrtc = {
+
+		uuid : null,
+
+		currentUser : null,
+
+		slide_moods 	: [
+			{ name : "Horrible" , count : 0, value : 0 },
+			{ name : "Bad"		, count : 0, value : 1 }, 
+			{ name : "Good"		, count : 0, value : 2 }, 
+			{ name : "Great"	, count : 0, value : 3 },
+			{ name : "Awesome"	, count : 0, value : 4 }
+		],
+
+		slide_pics 	: [
+			{ pic_url : "images/presentation/01.png" },
+			{ pic_url : "images/presentation/02.png" },
+			{ pic_url : "images/presentation/03.png" }
+		],
+
+		current_slide : 0,
+
+		channelMessages : [],
+
+		presentationVotes : [],
+
+		init : function(){
+
+			var self = agility_webrtc;
+			
+			self.loadTemplates({
+				templates_url : "javascripts/resources/templates.html"
+			}, function(){			
+
+				self.showPresentationScreen();
+
+			})
+
+
+		},
+
+		applyStyles 	: function(){
+
+  
+			var parHeight = $(window).height(); /*Get Screen Height*/
+			var parWidth = $(window).width(); /*Get Screen Width*/			
+
+			if(parWidth > 769){
+				$('.commentsWindowWrap .commentsList').css('height',parHeight-288); //Update Card Holder Height
+				$('.sliderWrap .slider').css('height',parHeight-364); /*Update Card Holder Height*/
+				$('.sliderWrap .sliderEspectador').css('height',parHeight-320); /*Update Card Holder Height*/
+			};
+
+			if(parWidth < 768){
+				$('.commentsWindowWrap .commentsList').css('height','auto'); /*Update Card Holder Height*/
+				$('.sliderWrap .slider').css('height','auto'); /*Update Card Holder Height*/
+				$('.sliderWrap .sliderEspectador').css('height','auto'); /*Update Card Holder Height*/
+			};
+
+		},
+		
+
+		showPresentationScreen : function(){
+
+			agility_webrtc.render({
+				container 	: "#content",
+				template 	: "#presentation_template",
+				data 		: {
+					user 		: agility_webrtc.currentUser,
+					slide_moods : agility_webrtc.slide_moods,
+					slides  : agility_webrtc.slide_pics
+				}
+			})	
+
+		},
+
+
+		render 				: function(options){
+
+			var content = _.template($(options.template).html(), options.data );
+
+			$(options.container).html(content);	
+
+		},
+
+		render_prepend		: function(options){
+
+			var content = _.template($(options.template).html(), options.data );
+
+			$(options.container).prepend(content);	
+
+		},
+		
+		loadTemplates : function(options, callback){
+
+			$("#templatesContainer").empty().remove();
+
+			$('<div id="templatesContainer"></div>').appendTo('body');
+			
+			$('#templatesContainer').load(("javascripts/resources/templates.html?r=" + Date.now()), function(){
+
+				if(typeof callback === "function"){
+					callback();
+				}
+
+			})
+
+		},
+
+		setInStore 	: function(item, key){
+
+			if(item == null){
+				return false;
+			}
+
+			item = _.isString(item) ? item : JSON.stringify(item);
+			
+			window.localStorage.setItem(key, item);
+
+		},
+		getFromStore : function(key){
+			
+			return JSON.parse(window.localStorage.getItem(key));
+
+		}
+
+	}
+    
+	agility_webrtc.init();
+
