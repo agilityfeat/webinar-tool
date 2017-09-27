302482a11015f91f9d6f7c128d18d223167ab517
Hector Zelaya <inge.zelaya@gmail.com>
2017-09-27 16:02:49 -0600

    upgraded express version and added shrinkwrap"

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,8 @@
 var routes = function (params) {
-
-	var app = params.app;
-
-	app.get('/', function(req, res){
-			res.render('index.ejs');
-		})
-
+    var app = params.app;
+    app.get('/', function (req, res) {
+        res.render('index.ejs');
+    })
 }
-
 module.exports = routes;
-
 console.log("All routes registered");
